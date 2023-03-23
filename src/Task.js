6f1d25cba23842eddbe825af12324cd426cf17b4
BerberKagan <kaganberbero@gmail.com>
2023-03-23 14:35:41 +0300

    All of the tasks are done

--- src/Task.js
+++ src/Task.js
@@ -1,11 +1,23 @@
 import React from "react";
+import { formatDistanceToNow, differenceInDays } from 'date-fns';
+import { tr } from 'date-fns/locale';
 
 const Task = ({ taskObj, onComplete }) => {
+
+  const dateToNow = formatDistanceToNow(new Date(taskObj.deadline), { locale: tr });
+  const diffDays = differenceInDays(new Date(taskObj.deadline), new Date());
+  const markAsRed = (diffDays <= 3) ? true : false;
 
   return (
     <div className="task">
       <h3>{taskObj.title}</h3>
-      <div className="deadline">son teslim: <span>{taskObj.deadline}</span></div>
+      <div className="deadline">
+        son teslim:
+        <span className={markAsRed ? "bg-[#ffd9d4]" : "bg-slate-300"}>
+          {dateToNow}
+        </span>
+        {/* kalan gün: {diffDays} */}
+      </div>
       <p>{taskObj.description}</p>
       <div>
         {taskObj.people.map((p) => (
